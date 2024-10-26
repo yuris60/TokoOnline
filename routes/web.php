526478e3b47d8d037abfe27d9013cc777031e7ee
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-10-26 12:16:03 +0700

    Add Route "User"

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,5 @@ Route::get('backend/beranda', [BerandaController::class, 'berandaBackend'])->nam
 Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('backend.login');
 Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
 Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
+
+Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
