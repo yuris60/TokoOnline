5dc3807ceff7d32a243a94c6dab92cd4c2f644a7
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-10-26 12:16:32 +0700

    Update File

--- resources/views/backend/v_layouts/app.blade.php
+++ resources/views/backend/v_layouts/app.blade.php
@@ -1,28 +1,319 @@
 <!DOCTYPE html>
-<html lang="en">
+<html dir="ltr" lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="keywords" content="Toko Online" />
+  <meta name="description" content="Project WP 2 Universitas Bina Sarana Informatika" />
+  <meta name="robots" content="noindex,nofollow" />
   <title>Toko Online</title>
+  <!-- Favicon icon -->
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('backend/image/icon_univ_bsi.png') }}" />
+  <!-- Custom CSS -->
+  <link rel="stylesheet" type="text/css"
+    href="{{ asset('backend/matrix-admin/assets/extra-libs/multicheck/multicheck.css') }}" />
+  <link href="{{ asset('backend/matrix-admin/assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.css') }}"
+    rel="stylesheet" />
+  <link href="{{ asset('backend/matrix-admin/dist/css/style.min.css') }}" rel="stylesheet" />
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 
 <body>
-  <a href="{{ route('backend.beranda') }}">Beranda</a>
-  <a href="#">User</a>
-  <a href="#" onclick="event.preventDefault(); document.getElementById('keluar-app').submit();">Keluar</a>
+  <!-- ============================================================== -->
+  <!-- Preloader - style you can find in spinners.css -->
+  <!-- ============================================================== -->
+  <div class="preloader">
+    <div class="lds-ripple">
+      <div class="lds-pos"></div>
+      <div class="lds-pos"></div>
+    </div>
+  </div>
+  <!-- ============================================================== -->
+  <!-- Main wrapper - style you can find in pages.scss -->
+  <!-- ============================================================== -->
+  <div id="main-wrapper" data-layout="vertical" data-navbarbg="skin5" data-sidebartype="full"
+    data-sidebar-position="absolute" data-header-position="absolute" data-boxed-layout="full">
+    <!-- ============================================================== -->
+    <!-- Topbar header - style you can find in pages.scss -->
+    <!-- ============================================================== -->
+    <header class="topbar" data-navbarbg="skin5">
+      <nav class="navbar top-navbar navbar-expand-md navbar-dark">
+        <div class="navbar-header" data-logobg="skin5">
+          <!-- ============================================================== -->
+          <!-- Logo -->
+          <!-- ============================================================== -->
+          <a class="navbar-brand" href="index.html">
+            <!-- Logo icon -->
+            <b class="logo-icon ps-2">
+              <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+              <!-- Dark Logo icon -->
+              <img src="{{ asset('backend/image/icon_univ_bsi.png') }}" alt="homepage" class="light-logo"
+                width="25" />
+            </b>
+            <!--End Logo icon -->
+            <!-- Logo text -->
+            <span class="logo-text ms-2">
+              <!-- dark Logo text -->
+              <img src="{{ asset('backend/image/logo_text.png') }}" alt="homepage" class="light-logo" />
+            </span>
+            <!-- Logo icon -->
+            <!-- <b class="logo-icon"> -->
+            <!--You can put here icon as well // <i class="wi wi-sunset"></i> //-->
+            <!-- Dark Logo icon -->
+            <!-- <img src="{{ asset('backend/matrix-admin/') }}assets/images/logo-text.png" alt="homepage" class="light-logo" /> -->
 
-  <p></p>
+            <!-- </b> -->
+            <!--End Logo icon -->
+          </a>
+          <!-- ============================================================== -->
+          <!-- End Logo -->
+          <!-- ============================================================== -->
+          <!-- ============================================================== -->
+          <!-- Toggle which is visible on mobile only -->
+          <!-- ============================================================== -->
+          <a class="nav-toggler waves-effect waves-light d-block d-md-none" href="javascript:void(0)"><i
+              class="ti-menu ti-close"></i></a>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End Logo -->
+        <!-- ============================================================== -->
+        <div class="navbar-collapse collapse" id="navbarSupportedContent" data-navbarbg="skin5">
+          <!-- ============================================================== -->
+          <!-- toggle and nav items -->
+          <!-- ============================================================== -->
+          <ul class="navbar-nav float-start me-auto">
+            <li class="nav-item d-none d-lg-block">
+              <a class="nav-link sidebartoggler waves-effect waves-light" href="javascript:void(0)"
+                data-sidebartype="mini-sidebar"><i class="mdi mdi-menu font-24"></i></a>
+            </li>
+          </ul>
+          <!-- ============================================================== -->
+          <!-- Right side toggle and nav items -->
+          <!-- ============================================================== -->
+          <ul class="navbar-nav float-end">
+            <!-- ============================================================== -->
+            <!-- User profile and search -->
+            <!-- ============================================================== -->
+            <li class="nav-item dropdown">
+              <a class="
+                    nav-link
+                    dropdown-toggle
+                    text-muted
+                    waves-effect waves-dark
+                    pro-pic
+                  "
+                href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="{{ asset('backend/matrix-admin/assets/images/users/1.jpg') }}" alt="user"
+                  class="rounded-circle" width="31" />
+              </a>
+              <ul class="dropdown-menu dropdown-menu-end user-dd animated" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-account me-1 ms-1"></i> My
+                  Profile</a>
+                <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-wallet me-1 ms-1"></i> My
+                  Balance</a>
+                <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-email me-1 ms-1"></i> Inbox</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="javascript:void(0)"><i class="mdi mdi-settings me-1 ms-1"></i> Account
+                  Setting</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="javascript:void(0)"><i class="fa fa-power-off me-1 ms-1"></i>
+                  Logout</a>
+                <div class="dropdown-divider"></div>
+                <div class="ps-4 p-10">
+                  <a href="javascript:void(0)" class="btn btn-sm btn-success btn-rounded text-white">View Profile</a>
+                </div>
+              </ul>
+            </li>
+            <!-- ============================================================== -->
+            <!-- User profile and search -->
+            <!-- ============================================================== -->
+          </ul>
+        </div>
+      </nav>
+    </header>
+    <!-- ============================================================== -->
+    <!-- End Topbar header -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <aside class="left-sidebar" data-sidebarbg="skin5">
+      <!-- Sidebar scroll-->
+      <div class="scroll-sidebar">
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav">
+          <ul id="sidebarnav" class="pt-4">
+            <li class="sidebar-item">
+              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('backend.beranda') }}"
+                aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span
+                  class="hide-menu">Dashboard</span></a>
+            </li>
+            <li class="sidebar-item">
+              <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ route('backend.user.index') }}"
+                aria-expanded="false"><i class="mdi mdi-account"></i><span class="hide-menu">User</span></a>
+            </li>
+            <li class="sidebar-item"> <a class="sidebar-link has-arrow waveseffect waves-dark"
+                href="javascript:void(0)" aria-expanded="false"><i class="mdi mdi-shopping"></i><span
+                  class="hide-menu">Data Produk </span></a>
+              <ul aria-expanded="false" class="collapse first-level">
+                <li class="sidebar-item"><a href="icon-material.html" class="sidebar-link"><i
+                      class="mdi mdi-chevron-right"></i><span class="hide-menu"> Kategori
+                    </span></a>
+                </li>
+                <li class="sidebar-item"><a href="icon-fontawesome.html" class="sidebar-link"><i
+                      class="mdi mdi-chevron-right"></i><span class="hide-menu"> Produk
+                    </span></a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!-- ============================================================== -->
+    <!-- End Left Sidebar - style you can find in sidebar.scss  -->
+    <!-- ============================================================== -->
+    <!-- ============================================================== -->
+    <!-- Page wrapper  -->
+    <!-- ============================================================== -->
+    <div class="page-wrapper">
+      <!-- ============================================================== -->
+      <!-- Bread crumb and right sidebar toggle -->
+      <!-- ============================================================== -->
+      {{-- <div class="page-breadcrumb">
+        <div class="row">
+          <div class="col-12 d-flex no-block align-items-center">
+            <h4 class="page-title">Tables</h4>
+            <div class="ms-auto text-end">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Home</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">
+                    Library
+                  </li>
+                </ol>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div> --}}
+      <!-- ============================================================== -->
+      <!-- End Bread crumb and right sidebar toggle -->
+      <!-- ============================================================== -->
+      <!-- ============================================================== -->
+      <!-- Container fluid  -->
+      <!-- ============================================================== -->
+      <div class="container-fluid">
+        <!-- ============================================================== -->
+        <!-- Start Page Content -->
+        <!-- ============================================================== -->
 
-  <!-- @yieldawal -->
-  @yield('content')
-  <!-- @yieldakhir -->
 
-  <!-- Keluar App -->
-  <form action="{{ route('backend.logout') }}" method="POST" id="keluar-app" class="d-none">
-    @csrf
-  </form>
+        <!-- @yieldAwal -->
+        @yield('content')
+        <!-- @yieldAkhir-->
+
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">Basic Datatable</h5>
+                <div class="table-responsive">
+                  <table id="zero_config" class="table table-striped table-bordered">
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Office</th>
+                        <th>Age</th>
+                        <th>Start date</th>
+                        <th>Salary</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Tiger Nixon</td>
+                        <td>System Architect</td>
+                        <td>Edinburgh</td>
+                        <td>61</td>
+                        <td>2011/04/25</td>
+                        <td>$320,800</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- ============================================================== -->
+        <!-- End PAge Content -->
+        <!-- ============================================================== -->
+        <!-- ============================================================== -->
+        <!-- Right sidebar -->
+        <!-- ============================================================== -->
+        <!-- .right-sidebar -->
+        <!-- ============================================================== -->
+        <!-- End Right sidebar -->
+        <!-- ============================================================== -->
+      </div>
+      <!-- ============================================================== -->
+      <!-- End Container fluid  -->
+      <!-- ============================================================== -->
+      <!-- ============================================================== -->
+      <!-- footer -->
+      <!-- ============================================================== -->
+      <footer class="footer text-center">
+        All Rights Reserved by Matrix-admin. Designed and Developed by
+        <a href="https://www.wrappixel.com">WrapPixel</a>.
+      </footer>
+      <!-- ============================================================== -->
+      <!-- End footer -->
+      <!-- ============================================================== -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Page wrapper  -->
+    <!-- ============================================================== -->
+  </div>
+  <!-- ============================================================== -->
+  <!-- End Wrapper -->
+  <!-- ============================================================== -->
+  <!-- ============================================================== -->
+  <!-- All Jquery -->
+  <!-- ============================================================== -->
+  <script src="{{ asset('backend/matrix-admin/assets/libs/jquery/dist/jquery.min.js') }}"></script>
+  <!-- Bootstrap tether Core JavaScript -->
+  <script src="{{ asset('backend/matrix-admin/assets/libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+  <!-- slimscrollbar scrollbar JavaScript -->
+  <script src="{{ asset('backend/matrix-admin/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jque') }}ry.min.js">
+  </script>
+  <script src="{{ asset('backend/matrix-admin/assets/extra-libs/sparkline/sparkline.js') }}"></script>
+  <!--Wave Effects -->
+  <script src="{{ asset('backend/matrix-admin/dist/js/waves.js') }}"></script>
+  <!--Menu sidebar -->
+  <script src="{{ asset('backend/matrix-admin/dist/js/sidebarmenu.js') }}"></script>
+  <!--Custom JavaScript -->
+  <script src="{{ asset('backend/matrix-admin/dist/js/custom.min.js') }}"></script>
+  <!-- this page js -->
+  <script src="{{ asset('backend/matrix-admin/assets/extra-libs/multicheck/datatable-checkbox-init.js') }}"></script>
+  <script src="{{ asset('backend/matrix-admin/assets/extra-libs/multicheck/jquery.multicheck.js') }}"></script>
+  <script src="{{ asset('backend/matrix-admin/assets/extra-libs/DataTables/datatables.min.js') }}"></script>
+  <script>
+    /****************************************
+     *       Basic Table                   *
+     ****************************************/
+    $("#zero_config").DataTable();
+  </script>
 </body>
 
 </html>
