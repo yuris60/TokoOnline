ed37c4bfab46aaf55bba96ab0feb4dc192885791
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-10-26 12:01:40 +0700

    Add Images

---
+++ resources/views/backend/v_user/index.blade.php
@@ -0,0 +1,42 @@
+@extends('backend.v_layouts.app')
+@section('content')
+  <h3>{{ $judul }}</h3>
+  <a href="{{ route('backend.user.create') }}">
+    <button type="button">Tambah</button>
+  </a>
+
+  <div class="card mt-4">
+    <div class="card-body">
+      <table class="table">
+        <tr>
+          <th>No</th>
+          <th>Email</th>
+          <th>Nama</th>
+          <th>Role</th>
+          <th>Status</th>
+          <th>Aksi</th>
+        </tr>
+        @foreach ($index as $row)
+          <tr>
+            <td>{{ $loop->iteration }}</td>
+            <td>{{ $row->nama }}</td>
+            <td>{{ $row->email }}</td>
+            <td>{{ $row->role }}</td>
+            <td>{{ $row->status }}</td>
+            <td>
+              <a href="{{ route('backend.user.edit', $row->id) }}">
+                <button type="button">Ubah</button>
+              </a>
+
+              <form action="{{ route('backend.user.destroy', $row->id) }}" method="POST">
+                @method('delete')
+                @csrf
+                <button type="submit">Hapus</button>
+              </form>
+            </td>
+          </tr>
+        @endforeach
+      </table>
+    </div>
+  </div>
+@endsection
