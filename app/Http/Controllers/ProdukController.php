98700f44e4f3334d706d9f8bde84f9f2a96509b9
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-11-27 06:51:34 +0700

    Add Controller "Produk"

---
+++ app/Http/Controllers/ProdukController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ImageHelper;
+use App\Models\Kategori;
+use App\Models\Produk;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ProdukController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $produk = Produk::orderBy('updated_at', 'desc')->get();
+        return view('backend.v_produk.index', [
+            'judul' => 'Data Produk',
+            'index' => $produk
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $kategori = Kategori::orderBy('nama_kategori', 'asc')->get();
+        return view('backend.v_produk.create', [
+            'judul' => 'Tambah Produk',
+            'kategori' => $kategori
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'kategori_id' => 'required',
+            'nama_produk' => 'required|max:255|unique:produk',
+            'detail' => 'required',
+            'harga' => 'required',
+            'berat' => 'required',
+            'stok' => 'required',
+            'foto' => 'required|image|mimes:jpeg,jpg,png,gif|file|max:1024',
+        ], $messages = [
+            'foto.image' => 'Format gambar gunakan file dengan ekstensi jpeg, jpg, png, atau gif.',
+            'foto.max' => 'Ukuran file gambar Maksimal adalah 1024 KB.'
+        ]);
+        $validatedData['status'] = 0;
+        $validatedData['user_id'] = Auth::user()->id;
+
+        if ($request->file('foto')) {
+            $file = $request->file('foto');
+            $extension = $file->getClientOriginalExtension();
+            $originalFileName = date('YmdHis') . '_' . uniqid() . '.' . $extension;
+            $directory = 'storage/img-produk/';
+            // Simpan gambar asli
+            $fileName = ImageHelper::uploadAndResize($file, $directory, $originalFileName);
+            $validatedData['foto'] = $fileName;
+            // create thumbnail 1 (lg)
+            $thumbnailLg = 'thumb_lg_' . $originalFileName;
+            ImageHelper::uploadAndResize($file, $directory, $thumbnailLg, 800, null);
+            // create thumbnail 2 (md)
+            $thumbnailMd = 'thumb_md_' . $originalFileName;
+            ImageHelper::uploadAndResize($file, $directory, $thumbnailMd, 500, 519);
+            // create thumbnail 3 (sm)
+            $thumbnailSm = 'thumb_sm_' . $originalFileName;
+            ImageHelper::uploadAndResize($file, $directory, $thumbnailSm, 100, 110);
+            // Simpan nama file asli di database
+            $validatedData['foto'] = $originalFileName;
+        }
+        Produk::create($validatedData, $messages);
+        return redirect()->route('backend.produk.index')->with('success', 'Data berhasil tersimpan');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
