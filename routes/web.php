66c1fd5bffea75098c8976c5a3ecf8fa687d3080
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-11-27 06:53:22 +0700

    Add Routes "Produk"

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BerandaController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\ProdukController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,4 @@ Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('
 
 Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
 Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
+Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
