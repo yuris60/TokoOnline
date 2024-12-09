c583adeb69224b17b5a2ce34a4cfbc43b6decdb6
Yuris Alkhalifi <49089111+yuris60@users.noreply.github.com>
2024-12-09 20:38:47 +0700

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,17 @@ Route::get('backend/login', [LoginController::class, 'loginBackend'])->name('bac
 Route::post('backend/login', [LoginController::class, 'authenticateBackend'])->name('backend.login');
 Route::post('backend/logout', [LoginController::class, 'logoutBackend'])->name('backend.logout');
 
+// Route User
 Route::resource('backend/user', UserController::class, ['as' => 'backend'])->middleware('auth');
-Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
-Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
+Route::get('backend/laporan/formuser', [UserController::class, 'formUser'])->name('backend.laporan.formuser')->middleware('auth');
+Route::post('backend/laporan/cetakuser', [UserController::class, 'cetakUser'])->name('backend.laporan.cetakuser')->middleware('auth');
 
-// Route untuk menambahkan foto
+// Route Kategori
+Route::resource('backend/kategori', KategoriController::class, ['as' => 'backend'])->middleware('auth');
+
+// Route Produk
+Route::resource('backend/produk', ProdukController::class, ['as' => 'backend'])->middleware('auth');
 Route::post('foto-produk/store', [ProdukController::class, 'storeFoto'])->name('backend.foto_produk.store')->middleware('auth');
-// Route untuk menghapus foto
 Route::delete('foto-produk/{id}', [ProdukController::class, 'destroyFoto'])->name('backend.foto_produk.destroy')->middleware('auth');
+Route::get('backend/laporan/formproduk', [ProdukController::class, 'formProduk'])->name('backend.laporan.formproduk')->middleware('auth');
+Route::post('backend/laporan/cetakproduk', [ProdukController::class, 'cetakProduk'])->name('backend.laporan.cetakproduk')->middleware('auth');
